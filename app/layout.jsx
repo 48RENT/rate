bd02fad52b1923d8d82dbf10e798588d36e851bf
48RENT <ram48rent@gmail.com>
2026-04-20 20:35:24 +0700

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "48RENT Camera Rental",
+  description: "Camera Rental Calculator",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="th">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
